9ab871db101a251ddc806e69b1cdf3717f1ba205
sust-mazhen <291540704@qq.com>
2019-01-28 15:31:41 +0800

    1)添加了第四章的横向偏差折线数据的代码 2)弄啥黎曼

--- HARRIS/harris.py
+++ HARRIS/harris.py
@@ -4,15 +4,14 @@
 import cv2
 import numpy as np
 
-filename = "D:\\11.jpg"
-img = cv2.imread("D:\\masterstu\\imageData\\img\\1.jpg")
+filename = "D:\\33.jpg"
+img = cv2.imread("D:\\masterstu\\imageData\\img\\3.jpg")
 gray = cv2.imread(filename, 0)
 gray = np.float32(gray)
-
 # 输入图像必须是float32，最后一个参数在0.04 到0.05 之间
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 dst = cv2.dilate(dst, None)
-img[dst > 0.005*dst.max()] = [0, 0, 255]
+img[dst > 0.005 * dst.max()] = [0, 0, 255]
 cv2.imshow('dst', img)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
--- HARRIS/harris_num.py
+++ HARRIS/harris_num.py
@@ -5,22 +5,20 @@ import matplotlib.pyplot as plt
 from pylab import *
 
 mpl.rcParams['font.sans-serif'] = ['SimHei']
-names = ['5', '10', '15', '20', '25']
+names = ['0.01', '0.02', '0.03', '0.04', '0.05']
 x = range(len(names))
 y = [700, 750, 770, 850, 900]
-y1 = [800, 850, 900, 950, 1000]
-# plt.plot(x, y, 'ro-')
-# plt.plot(x, y1, 'bo-')
-# pl.xlim(-1, 11)  # 限定横轴的范围
-# pl.ylim(-1, 110)  # 限定纵轴的范围
-plt.plot(x, y, marker='o', mec='r', mfc='w', label="SUSAN角点检测")
-plt.plot(x, y1, marker='*', ms=10, label="Harris角点检测")
-plt.legend()  # 让图例生效
+y1 = [750, 870, 899, 977, 1020]
+# plt.plot(x, y, marker='o', mec='r', mfc='w', label="SUSAN角点检测")
+# plt.plot(x, y1, marker='*', mec='g', mfc='w', ms=10, label="Harris角点检测")
+# plt.plot(x, y, marker='o', mec='r', mfc='w')
+plt.plot(x, y1, marker='*', mec='g', mfc='w', ms=10)
+# plt.legend()  # 让图例生效
 plt.xticks(x, names, rotation=0)
 plt.margins(0)
 plt.subplots_adjust(bottom=0.15)
-plt.xlabel("参数k")  # X轴标签
+plt.xlabel("参数tg")  # X轴标签
 plt.ylabel("角点数量")  # Y轴标签
-plt.title("角点检测数量随参数变化")  # 标题
+plt.title("SUSAN角点检测数量随参数变化")  # 标题
 
 plt.show()
